10139320427588385e923868df5597a11c8fbb2b
Sabasalfredo Mejia <sabasalfredo@gmail.com>
2022-01-12 15:54:16 -0800

    added procfile

--- app/main.py
+++ app/main.py
@@ -43,5 +43,5 @@ app.include_router(vote.router)
 @app.get("/")
 def root():
     # return {"message": "Welcome to my api"}
-    return {"message": "Hello World!"}
+    return {"message": "Hello World Always!"}
 
